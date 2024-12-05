bbbeb13cf4e3f3436d2b572d39570576d233d945
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-12-05 14:32:00 +0100

    update

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
+import os
+import adif_io
+
+
+def get_all_qsos(input_paths):
+    print(f"Found {len(input_paths)} ADIF files: ")
+
+    ret_qsos = []
+
+    for t_fp in input_paths:
+        t_qsos, _ = adif_io.read_from_file(C_WORK_DATA_DIR + "input/" + t_fp)
+        print(f" - '{t_fp}' with {len(t_qsos)} QSOs")
+        for t_qso in t_qsos:
+            ret_qsos.append(t_qso)
+
+    return ret_qsos
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    C_WORK_DATA_DIR = "workData/"
+
+    print("########################################")
+    print("[ADIF LOG ANALYZER]")
+    print("########################################")
+
+    # Load and parse ADIF files
+    print("\n[LOAD AND PARSE ADIF FILES]\n")
+
+    # Check if work data dir is present
+    assert os.path.exists(C_WORK_DATA_DIR), "Error: Work Data Dir not found"
+
+    # Get all ADIF files in input dir
+    adif_input_files = [f for f in os.listdir(C_WORK_DATA_DIR + "input/", ) if
+                        f.endswith(".adi") or f.endswith(".adif")]
+
+    assert len(adif_input_files) > 0, "Error: No ADIF files found in Input Dir"
+
+    all_qsos = get_all_qsos(adif_input_files)
+
+    del adif_input_files
+
+    assert len(all_qsos) > 0, "Error: No QSOs found in all ADIF files"
+
+    # Iterate over all QSOs
+    # for t_qso in all_qsos[:1]:
+    #     print(t_qso)
+    # del t_qso
+
+    # Output logbook overview
+    print("\n[Logbook Overview]\n")
+
+    print(f"Found {len(all_qsos)} QSOs in total")
