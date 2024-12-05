ffbebc7573df766dfe4126c7d2974f4f966e3d4d
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-12-05 14:38:06 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import os
+from dataclasses import dataclass
+
 import adif_io
+
+
+@dataclass
+class QsoEntity:
+    call: str
 
 
 def get_all_qsos(input_paths):
@@ -16,6 +23,17 @@ def get_all_qsos(input_paths):
     return ret_qsos
 
 
+def get_all_qsos_ent(input_qsos) -> [QsoEntity]:
+    ret_qsos: [QsoEntity] = []
+
+    for t_qso in input_qsos:
+        ret_qsos.append(QsoEntity(
+            call=t_qso["call"]
+        ))
+
+    return ret_qsos
+
+
 if __name__ == "__main__":
     C_WORK_DATA_DIR = "workData/"
 
@@ -28,25 +46,19 @@ if __name__ == "__main__":
 
     # Check if work data dir is present
     assert os.path.exists(C_WORK_DATA_DIR), "Error: Work Data Dir not found"
-
     # Get all ADIF files in input dir
     adif_input_files = [f for f in os.listdir(C_WORK_DATA_DIR + "input/", ) if
                         f.endswith(".adi") or f.endswith(".adif")]
-
     assert len(adif_input_files) > 0, "Error: No ADIF files found in Input Dir"
 
     all_qsos = get_all_qsos(adif_input_files)
-
     del adif_input_files
-
     assert len(all_qsos) > 0, "Error: No QSOs found in all ADIF files"
 
-    # Iterate over all QSOs
-    # for t_qso in all_qsos[:1]:
-    #     print(t_qso)
-    # del t_qso
+    all_qsos_ent: [QsoEntity] = get_all_qsos_ent(all_qsos)
+    del all_qsos
 
     # Output logbook overview
     print("\n[Logbook Overview]\n")
 
-    print(f"Found {len(all_qsos)} QSOs in total")
+    print(f"Found {len(all_qsos_ent)} QSOs in total")
